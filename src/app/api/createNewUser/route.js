39b31a67af005114a76a7599fb0625a3d293b81e
Kaung Phyo Wai <53686958+kaungphyowai@users.noreply.github.com>
2024-07-17 06:47:58 -0700

    fix the build error

--- src/app/api/createNewUser/route.js
+++ src/app/api/createNewUser/route.js
@@ -1,44 +1,23 @@
-// currency: [
-//     {
-//         id: "..",
-//         name: "..."
-//     }
-// ]
+export const dynamic = 'force-dynamic' // defaults to force-static
 
-export async function GET(request) {
-  console.log(process.env.AIRTABLE_TOKEN)
+export async function POST(request) {
+    const text = await request.text()
+    
 
-  //get the wallet data base
+    var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Authorization", `Bearer ${process.env.AIRTABLE_TOKEN}`);
+myHeaders.append("Cookie", "brw=brwb8tzTqMzSEOnxJ");
 
-  var myHeaders = new Headers();
-  myHeaders.append("Authorization", `Bearer ${process.env.AIRTABLE_TOKEN}`);
-  myHeaders.append("Cookie", "brw=brwb8tzTqMzSEOnxJ; AWSALB=9ZYiIiD0R1y3BfmkYAiHW3+nOoVCGqeDgFqmGShD1QeRLL3+wePNKkQdW0i2lKZDW+nBA28yxhXkzgZc2tIx+KJkdMEko/25dX+PH982RFbwXOaJCWaPd6d0l4bF; AWSALBCORS=9ZYiIiD0R1y3BfmkYAiHW3+nOoVCGqeDgFqmGShD1QeRLL3+wePNKkQdW0i2lKZDW+nBA28yxhXkzgZc2tIx+KJkdMEko/25dX+PH982RFbwXOaJCWaPd6d0l4bF");
 
-  var requestOptions = {
-  method: 'GET',
+var requestOptions = {
+  method: 'POST',
   headers: myHeaders,
+  body: text,
   redirect: 'follow'
-  };
+};
 
-  const response = await fetch("https://api.airtable.com/v0/appI7DFXUC7sezXwg/tblSRWE7HJcZKXGyP", requestOptions)
- const json = await response.json();
- let records = json.records;
- let answer = {};
- for(let record = 0; record < records.length; record++)
- {
-      let currency = records[record].fields["Currency"];
-      let wallet = records[record].fields["wallet_name"];
-      let id = records[record].id;
-
-      //if this key is not, create one
-      !Array.isArray(answer[currency]) && (answer[currency] = new Array());
-
-      // push concering wallet to currency
-      answer[currency].push({
-          id: id,
-          name: wallet
-      })
- }
-
-  return Response.json(answer);
+let response = await fetch("https://api.airtable.com/v0/appI7DFXUC7sezXwg/tblF0xccXeUryzaL1", requestOptions)
+  let json = await response.json();
+    return Response.json(response)
 }
