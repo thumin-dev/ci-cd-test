744588bd0e83494a22e021408d3fb2d28f7114d2
aye-nyeinSan <aye2904@gmail.com>
2024-07-31 23:23:16 +0700

    loadSupportRegion api added

---
+++ src/app/api/loadSupportRegion/route.js
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import db from "../../utilites/db";
+
+
+async function loadSupportRegion() {
+  const query = `SELECT * FROM SupportRegion`;
+  try {
+    const result = await db(query); 
+    console.log("Result for Support Region: ", result);
+    return result;
+  } catch (error) {
+    console.error(
+      "[DB] Can't show all available support regions From DB Query:",
+      error
+    );
+    throw error; 
+  }
+}
+
+// GET handler for the API route
+export async function GET(req) {
+  try {
+    const data = await loadSupportRegion();
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("[Error] Can't show all available support regions:", error);
+    return NextResponse.json(
+      { error: "Can't show all available support regions" },
+      { status: 500 }
+    );
+  }
+}
