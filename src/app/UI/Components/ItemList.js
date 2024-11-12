d5f80714e8d74b87d820bd6edb7efd221c325bef
aye-nyeinSan <aye2904@gmail.com>
2024-11-12 18:15:40 +0700

    paymentdetail and route works

--- src/app/UI/Components/ItemList.js
+++ src/app/UI/Components/ItemList.js
@@ -1,47 +1,18 @@
-import { Box, Button, Stack, Typography } from "@mui/material";
+import React from "react";
+import { Stack, Box, Typography, Chip } from "@mui/material";
 import ItemCard from "./ItemCard";
 
-function ItemList({ items, onLoadMore, searchQuery }) {
-  // Determine if there is a search query
-  const hasSearchQuery = searchQuery && searchQuery.trim() !== "";
-
+function ItemList({ items, searchQuery, onItemClick }) {
   return (
-    <Box
-      sx={{
-        width: 280,
-        height: "80vh",
-        backgroundColor: "#ffffff",
-        borderRadius: 3,
-        padding: 2,
-        overflowY: "auto",
-        boxShadow: 2,
-      }}
-    >
-      {/* Render the list of items */}
-      {items.length > 0 ? (
-        <Stack spacing={2}>
-          {items.map((item, index) => (
-            <ItemCard key={index} item={item} />
-          ))}
-        </Stack>
-      ) : (
-        <Typography variant="body2" align="center" sx={{ mt: 4 }}>
-          No items available
-        </Typography>
-      )}
-
-      {/* Conditionally render the "Load More..." button only if not searching */}
-      {!hasSearchQuery && items.length > 0 && (
-        <Button
-          fullWidth
-          variant="outlined"
-          sx={{ marginTop: 2 }}
-          onClick={onLoadMore}
-        >
-          Load More...
-        </Button>
-      )}
-    </Box>
+    <Stack spacing={2}>
+      {items.map((item) => (
+        <ItemCard
+          key={item.HopeFuelID}
+          item={item}
+          onClick={() => onItemClick(item.HopeFuelID)}
+        />
+      ))}
+    </Stack>
   );
 }
 
