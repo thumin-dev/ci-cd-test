91f269a9e712d1dbbd757de54dfaf2caf0f7aa39
Kaung Phyo Wai <53686958+kaungphyowai@users.noreply.github.com>
2024-12-05 02:44:42 -0800

    solve the screenshot error

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "hopefuel": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "hopefuel3393c0f6": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "devimages": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/app/utilites/getScreenShotUrl.js
+++ src/app/utilites/getScreenShotUrl.js
@@ -1,35 +1,25 @@
-import { getUrl } from 'aws-amplify/storage';
+import { getUrl } from "aws-amplify/storage";
 
-export default async function getScreenShotUrl(url)
-{   
-    console.log(url)
-    const regex = /public\/.+/g;
-    const found = url.match(regex);
-    let path = null
-    if(Array.isArray(found))
-    {
-        if(found.length > 0)
-        {
-            path = found[0]
+export default async function getScreenShotUrl(url) {
+  const regex = /public\/.+/g;
+  const found = url.match(regex);
+  let path = null;
+  if (Array.isArray(found)) {
+    if (found.length > 0) {
+      path = found[0];
+    } else {
+      return;
+    }
+  } else {
+    return;
+  }
+  // const path = "public/f7e4134b-2d85-4ed4-bbb2-d769a5ad1543Image 3-7-2567 BE at 10.54.jpeg";
 
-        }
-        else
-        {
-            return
-        }
-    }
-    else
-    {
-        return
-    }
-    // const path = "public/f7e4134b-2d85-4ed4-bbb2-d769a5ad1543Image 3-7-2567 BE at 10.54.jpeg";
-    const linkToStorageFile = await getUrl({
-      path: path,
-      
-      // Alternatively, path: ({identityId}) => `album/{identityId}/1.jpg`
-    });
-    console.log('signed URL: ', linkToStorageFile.url);
-    console.log('URL expires at: ', linkToStorageFile.expiresAt);
-    return linkToStorageFile.url;
+  const linkToStorageFile = await getUrl({
+    path: path,
+    // Alternatively, path: ({identityId}) => `album/{identityId}/1.jpg`
+  });
+  console.log("signed URL: ", linkToStorageFile.url);
+  console.log("URL expires at: ", linkToStorageFile.expiresAt);
+  return linkToStorageFile.url;
 }
- 
