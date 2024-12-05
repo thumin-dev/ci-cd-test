1c76abc5041e40677a407e56e78c6afe1edf187e
Kaung Phyo Wai <53686958+kaungphyowai@users.noreply.github.com>
2024-12-05 02:44:42 -0800

    solve the screenshot error

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 "use client";
 
 import React from "react";
+import awsconfig from "../aws-exports";
 import App from "./App";
-
+import { Amplify } from "aws-amplify";
+Amplify.configure(awsconfig);
 export default function Page() {
   return <App />;
 }
