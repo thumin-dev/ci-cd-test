05bc53043d46a112e461b2190ddc699e6b5a8437
Kaung Phyo Wai <53686958+kaungphyowai@users.noreply.github.com>
2024-12-22 16:22:03 -0800

    appBar changes according to the userRole

--- src/app/layout.js
+++ src/app/layout.js
@@ -41,7 +41,7 @@ export default function RootLayout({ children }) {
               <CssBaseline />
               <AgentProvider>
                 <UserProvider>
-                  <ResponsiveAppBar userRole={"admin"} />
+                  <ResponsiveAppBar />
                   {children}
                   {console.log(user)}
                 </UserProvider>
