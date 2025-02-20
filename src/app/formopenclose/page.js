1601d1639aa668cd0a79200ca202d476f69813c0
Aung Kaung Myat <115795473+Aung-kaung-myat-coding@users.noreply.github.com>
2025-02-20 14:30:53 +0700

    feat: implement other role view ui when admin close the forms, and move FormStatus to formopenclose/components/FormStatus.js

--- src/app/formopenclose/page.js
+++ src/app/formopenclose/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
-import FormStatus from "../UI/Components/FormStatus";
+import FormStatus from "./components/FormStatus";
 import { HISTORY_DATA } from "../variables/const";
 
 const FormOpenClosePage = () => {
