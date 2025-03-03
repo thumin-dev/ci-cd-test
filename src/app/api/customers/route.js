d480e74a4e0a587e786428189ef36c38a23a9781
AyeNyeinSan <56792505+aye-nyeinSan@users.noreply.github.com>
2025-03-03 10:29:42 +0700

    Add: customer detail api is added (#258)

--- src/app/api/customers/route.js
+++ src/app/api/customers/route.js
@@ -24,7 +24,7 @@ export async function GET(req) {
   try {
    
     const  [{total}] = await db(`SELECT COUNT(*) AS total FROM Customer`);
-    console.log("totalPages::", total);
+   // console.log("totalPages::", total);
 
     const customers = await fetchCustomers(limit, offset);
 
