044f0af68433f3954479a821741f7a6c0fbb0347
akm-coding <aungkaungmyatyeu123@gmail.com>
2025-03-10 15:30:24 +0700

    integrate: admin form open close api

--- src/app/createForm/checkUser.js
+++ src/app/createForm/checkUser.js
@@ -7,15 +7,34 @@ import {
   TextField,
   Typography,
 } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import checkUserSubmit from "../utilites/checkUserSubmit";
 import { getCurrentUser } from "aws-amplify/auth";
 import { useUser } from "../context/UserContext";
+import ServiceUnavailable from "../UI/Components/ServiceUnavailable";
 
 export default function CheckUser({ onUserCheck, userRole }) {
   const [loading, setLoading] = useState(false);
   const [hasPermissionThisMonth, sethasPermissionThisMonth] = useState(true);
+  const [isFormOpen, setIsFormOpen] = useState(false);
   const { currentUser } = useUser();
+
+  useEffect(() => {
+    const fetchFormStatus = async () => {
+      try {
+        const response = await fetch("/api/formOpenClose");
+        const result = await response.json();
+        setIsFormOpen(result.data[0].IsFormOpen);
+      } catch (error) {
+        console.error("Error fetching form status:", error);
+        setError("Failed to fetch form status");
+        setSnackbarOpen(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchFormStatus();
+  }, []);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -83,30 +102,34 @@ export default function CheckUser({ onUserCheck, userRole }) {
     </Box>
   ) : (
     <>
-      <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
-        <TextField
-          autoFocus
-          margin="normal"
-          required
-          fullWidth
-          name="name"
-          label="Name"
-          type="text"
-          id="name"
-        />
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          name="email"
-          label="Email Address"
-          type="email"
-          id="email"
-        />
-        <Button type="submit" fullWidth variant="contained" sx={{ mt: 3 }}>
-          Check User
-        </Button>
-      </Box>
+      {isFormOpen ? (
+        <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <TextField
+            autoFocus
+            margin="normal"
+            required
+            fullWidth
+            name="name"
+            label="Name"
+            type="text"
+            id="name"
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="email"
+            label="Email Address"
+            type="email"
+            id="email"
+          />
+          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3 }}>
+            Check User
+          </Button>
+        </Box>
+      ) : (
+        <ServiceUnavailable />
+      )}
       {hasPermissionThisMonth == false && (
         <h1>This user don't have permission this month anymore</h1>
       )}
--- src/app/formopenclose/page.js
+++ src/app/formopenclose/page.js
@@ -1,18 +1,92 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import FormStatus from "./components/FormStatus";
 import { HISTORY_DATA } from "../variables/const";
+import { useAgent } from "../context/AgentContext";
+import { Alert, Snackbar } from "@mui/material";
 
 const FormOpenClosePage = () => {
+  const agentId = useAgent();
   const [isFormOpen, setIsFormOpen] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [formVisibilityData, setFormVisibilityData] = useState([]);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchFormStatus = async () => {
+      try {
+        const response = await fetch("/api/formOpenClose");
+        const result = await response.json();
+        setFormVisibilityData(result.data);
+        setIsFormOpen(result.data[0].IsFormOpen);
+      } catch (error) {
+        console.error("Error fetching form status:", error);
+        setError("Failed to fetch form status");
+        setSnackbarOpen(true);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchFormStatus();
+  }, []);
+
+  const handleFormOpenClose = async (newFormStatus) => {
+    try {
+      const myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      const raw = JSON.stringify({
+        AgentId: agentId,
+        IsFormOpen: newFormStatus,
+      });
+
+      const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+      };
+      const response = await fetch("api/formOpenClose", requestOptions);
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      setIsFormOpen(newFormStatus);
+    } catch (error) {
+      console.error("Error updating form status:", error);
+      setError("Failed to update form status");
+      setSnackbarOpen(true);
+      setIsFormOpen(!newFormStatus);
+    }
+  };
+
+  const handleCloseSnackbar = () => {
+    setSnackbarOpen(false);
+  };
+
   return (
     <>
       <FormStatus
         isFormOpen={isFormOpen}
-        setIsFormOpen={setIsFormOpen}
-        data={HISTORY_DATA}
+        setIsFormOpen={handleFormOpenClose}
+        data={formVisibilityData}
       />
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+      >
+        <Alert
+          onClose={handleCloseSnackbar}
+          severity="error"
+          sx={{ width: "100%" }}
+        >
+          {error}
+        </Alert>
+      </Snackbar>
     </>
   );
 };
